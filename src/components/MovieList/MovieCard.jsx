2cf03885bf124c827982bc15d48a453ad9e6094a
Noorullah Noori <164587782+noorinoorullah130@users.noreply.github.com>
2025-03-21 11:45:20 +0430

    design is done

---
+++ src/components/MovieList/MovieCard.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import "./MovieCard.css";
+import Star from "../../assets/star.png";
+
+const MovieCard = () => {
+    return (
+        <a href="" className="movie_card">
+            <img
+                src="https://www.washingtonpost.com/graphics/2019/entertainment/oscar-nominees-movie-poster-design/img/black-panther-web.jpg"
+                alt="movie_poster"
+                className="movie_poster"
+            />
+
+            <div className="movie_details">
+                <h3 className="align_center movie_details_heading">Movie Name</h3>
+                <div className="movie_date_rate">
+                    <p>10-20-2020</p>
+                    <p>
+                        8.0{" "}
+                        <img
+                            src={Star}
+                            alt="rating icon"
+                            className="card_emoji"
+                        />
+                    </p>
+                </div>
+                <p className="movie_description">
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                    Incidunt fugiat iure illum nemo facere quos?
+                </p>
+            </div>
+        </a>
+    );
+};
+
+export default MovieCard;
